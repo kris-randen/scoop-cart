c954d8c566983186daf1da7d22b8cce8fbaaf8fa
kris-randen <kris.randen@gmail.com>
2024-01-09 00:18:31 +0530

    Initial commit. Project scoop-cart created.

---
+++ scoop-cart/scoop-cart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scoop-cart
+//
+//  Created by Krishnaswami Rajendren on 1/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
