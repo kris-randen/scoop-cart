3b2e6cc49cac60b3df80e0c4a81bd3327eeaea42
kris-randen <kris.randen@gmail.com>
2024-01-09 01:15:25 +0530

    Completed GroceryListView. Now on to AddItemView.

---
+++ scoop-cart/scoop-cart/Views/GroceryListView.swift
@@ -0,0 +1,58 @@
+//
+//  GroceryListView.swift
+//  scoop-cart
+//
+//  Created by Krishnaswami Rajendren on 1/9/24.
+//
+
+import SwiftUI
+
+struct GroceryListView: View {
+    @StateObject var vm = GroceryListViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(vm.items) { item in
+                    HStack {
+                        Text(item.name)
+                        Spacer()
+                        Text(item.quantityDescription)
+                    }
+                }
+                .onDelete(perform: deleteItems)
+            }
+            .navigationTitle("Grocery List")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: add) {
+                        Label("Add Item", systemImage: "plus")
+                    }
+                }
+            }
+        }
+    }
+    
+    private func deleteItems(at offsets: IndexSet) {
+        offsets.map {vm.items[$0].id}.forEach(vm.remove)
+    }
+    
+    private func add() {
+        /// Implementation for adding a new item
+    }
+}
+
+extension GroceryItem {
+    var quantityDescription: String {
+        switch quantity {
+        case .mass(let mass):
+            return "\(mass.value) \(mass.unit.rawValue)"
+        case .volume(let volume):
+            return "\(volume.value) \(volume.unit.rawValue)"
+        }
+    }
+}
+
+#Preview {
+    GroceryListView()
+}
